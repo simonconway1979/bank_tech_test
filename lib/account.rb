45eedf9b3072177ec9a774835aabf59a9ac40eba
Simon <simon.conway@condaal.com>
2017-01-23 10:45:11 +0000

    Added a method that deposits a value

--- lib/account.rb
+++ lib/account.rb
@@ -2,15 +2,16 @@ require 'pry'
 
 class Account
 
-# attr_reader :balance
-
-
 def initialize
 @balance = 0
 end
 
 def balance
   @balance
+end
+
+def deposit(amount)
+  @balance += amount
 end
 
 # binding.pry
